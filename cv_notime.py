6073cb9bcc30ad7232b6cef1eecfaa15e8ec07e6
Alexandru Meterez <ameterez@eu-login-19.euler.ethz.ch>
2022-03-06 18:53:03 +0100

    fixed no time

--- cv_notime.py
+++ cv_notime.py
@@ -1,7 +1,7 @@
 #This is the Fully Connected Model with leave-one-site-out cross-validation
 
 from model.model_notime import Model
-from preprocess import prepare_df
+from preprocess_new import prepare_df
 from sklearn.metrics import r2_score
 import torch
 import pandas as pd
@@ -9,80 +9,82 @@ import argparse
 import torch.nn.functional as F
 import numpy as np
 import operator
-from plotly import graph_objects as go
+# from plotly import graph_objects as go
 import pickle 
 
-
-# Parse arguments 
-parser = argparse.ArgumentParser(description='CV DNN')
- 
-parser.add_argument('-gpu', '--gpu', default=None, type=str,
-                      help='indices of GPU to enable ')
-
-parser.add_argument('-e', '--n_epochs', default=None, type=int,
-                      help='number of cv epochs ()')
-
-
-args = parser.parse_args()
-DEVICE = torch.device("cuda:" + args.gpu)
 torch.manual_seed(40)
+INPUT_FEATURES = 11
 
-#importing data
-data = pd.read_csv('utils/df_imputed.csv', index_col=0)
-data = data.drop(columns='date')
-raw = pd.read_csv('data/df_20210510.csv', index_col=0)['GPP_NT_VUT_REF']
-raw = raw[raw.index != 'CN-Cng']
-sites = raw.index.unique()
-
-masks = []
-for s in sites:
-    mask = raw[raw.index == s].isna().values
-    masks.append(list(map(operator.not_, mask)))
+if __name__ == '__main__':
+    # Parse arguments 
+    parser = argparse.ArgumentParser(description='CV DNN') 
+    parser.add_argument('--device', default=None, type=str,
+                          help='indices of GPU to enable ')
+    parser.add_argument('-e', '--n_epochs', default=None, type=int,
+                          help='number of cv epochs ()')
 
 
-df_sensor, df_meta, df_gpp = prepare_df(data)
+    args = parser.parse_args()
+    DEVICE = args.device
 
-INPUT_FEATURES = len(df_sensor[0].columns) 
+    #importing data
+    data = pd.read_csv('./utils/df_imputed.csv', index_col=0)
+    data = data.drop(columns='date')
+    raw = pd.read_csv('./data/df_20210510.csv', index_col=0)['GPP_NT_VUT_REF']
+    raw = raw[raw.index != 'CN-Cng']
+    sites = raw.index.unique()
 
-cv_r2 = []
-cv_pred = []
-for s in range(len(df_sensor)):
-    sites_to_train = list(range(len(df_sensor)))
-    
-    #leave the site out for cross-validation
-    sites_to_train.remove(s)
+    masks = []
+    for s in sites:
+        mask = raw[raw.index == s].isna().values
+        masks.append(list(map(operator.not_, mask)))
 
-    #prepare training and testing set
-    x_train = [df_sensor[i].values for i in sites_to_train]
-    y_train = [df_gpp[i].values.reshape(-1,1) for i in sites_to_train]
-
-    x_test = df_sensor[s].values 
-    y_test = df_gpp[s].values.reshape(-1,1) 
-
-    #import the model
-    model = Model(INPUT_FEATURES).to(DEVICE)
-    optimizer = torch.optim.Adam(model.parameters())
-
-    r2 = []
-    pred = []
-    
-    for epoch in range(args.n_epochs):
-        train_loss = 0.0
-        train_r2 = 0.0
-        model.train()
-        for (x, y) in zip(x_train, y_train):
-            x = torch.FloatTensor(x).to(DEVICE)
-            y = torch.FloatTensor(y).to(DEVICE)
-            y_pred = model(x)
-            optimizer.zero_grad()
-            loss = F.mse_loss( y_pred, y)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-            train_r2 += r2_score(y_true=y.detach().cpu().numpy(), y_pred=y_pred.detach().cpu().numpy())
+    cv_r2 = []
+    cv_pred = []
+    for s in range(len(sites)):
+        sites_to_train_list = list(range(len(sites)))
+        sites_to_train_list.remove(s)
+        sites_to_train=sites[sites_to_train_list]
+        site_to_test=sites[s]
         
-        model.eval()
-        with torch.no_grad():
+        #Prepare and standardise the sensor data
+        df_train=[data[data.index == site] for site in sites_to_train]
+        df_train=pd.concat(df_train)
+        df_test=data[data.index ==site_to_test]
+
+        df_sensor, df_sensor_test, df_gpp, df_gpp_test = prepare_df(df_train,df_test)
+        
+        #Prepare dataframe for training
+        x_train = [df_sensor[i].values for i in range(len(sites)-1)]
+        y_train = [df_gpp[i].values.reshape(-1,1) for i in range(len(sites)-1)]
+
+        x_test = df_sensor_test[0].values 
+        y_test = df_gpp_test[0].values.reshape(-1,1) 
+
+        #import the model
+        model = Model(INPUT_FEATURES).to(DEVICE)
+        optimizer = torch.optim.Adam(model.parameters())
+
+        r2 = []
+        pred = []
+        
+        for epoch in range(args.n_epochs):
+            train_loss = 0.0
+            train_r2 = 0.0
+            model.train()
+            for (x, y) in zip(x_train, y_train):
+                x = torch.FloatTensor(x).to(DEVICE)
+                y = torch.FloatTensor(y).to(DEVICE)
+                y_pred = model(x)
+                optimizer.zero_grad()
+                loss = F.mse_loss( y_pred, y)
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.item()
+                train_r2 += r2_score(y_true=y.detach().cpu().numpy(), y_pred=y_pred.detach().cpu().numpy())
+            
+            model.eval()
+            with torch.no_grad():
                 x = torch.FloatTensor(x_test).to(DEVICE)
                 y = torch.FloatTensor(y_test).to(DEVICE)
                 y_pred = model(x)
@@ -90,18 +92,16 @@ for s in range(len(df_sensor)):
                 test_r2 = r2_score(y_true=y.detach().cpu().numpy()[masks[s]], y_pred=y_pred.detach().cpu().numpy()[masks[s]])
                 r2.append(test_r2)
                 pred.append(y_pred)
-    
-    cv_r2.append(max(r2))
-    cv_pred.append(pred[np.argmax(r2)].detach().cpu().numpy())
-    print(f"Test Site: {s} R2: {cv_r2[s]}")
-    print("CV R2 cumulative mean: ", np.mean(cv_r2), " +- ", np.std(cv_r2))
-    print("-------------------------------------------------------------------")
-    
-#save the prediction dataframe
-d = {"site": sites, "preds": cv_pred}
-#df = pd.DataFrame(d)
-#df.to_csv("fcn_notime_predictions.csv")
-file_pi = open('DNN_predictions.pkl', 'wb') 
-pickle.dump(d, file_pi, pickle.HIGHEST_PROTOCOL)
+        
+        cv_r2.append(max(r2))
+        cv_pred.append(pred[np.argmax(r2)].detach().cpu().numpy())
+        print(f"Test Site: {s} R2: {cv_r2[s]}")
+        print("CV R2 cumulative mean: ", np.mean(cv_r2), " +- ", np.std(cv_r2))
+        print("-------------------------------------------------------------------")
+        
+    #save the prediction dataframe
+    d = {"Site": sites, "Predictions": cv_pred}
+    df = pd.DataFrame(d)
+    df.to_csv(f"notime_epochs_{args.n_epochs}.csv")
 
 
